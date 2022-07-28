e28584b52b86245fe5498079f3550d94d84560bc
Mateo Torres <torresmateo@gmail.com>
2022-07-28 13:58:20 -0300

    added resnik term wise

--- golib/core/semantic_similarity.py
+++ golib/core/semantic_similarity.py
@@ -3,6 +3,7 @@ import numpy as np
 import numpy.typing as npt
 
 from golib.core.gene_ontology import GeneOntology
+from golib.core.goterm import lowest_common_ancestor
 from typing import Dict
 
 class SemanticSimilarityMixin(metaclass=abc.ABCMeta):
@@ -30,13 +31,28 @@ class SemanticSimilarityMixin(metaclass=abc.ABCMeta):
 
 class Resnik(SemanticSimilarityMixin):
 
+    _ancestor_cache: Dict = {}
+
     def term_wise(self, ontology: GeneOntology, 
                   organism_name: str) -> Dict[str, npt.NDArray]:
+        ss = {}
+        for domain in GeneOntology.DOMAINS:
+            terms = [t for t in ontology._terms if t.domain == domain]
+            N = len(terms)
+            ss[domain] = np.zeros((N,N))
+            for i in range(N):
+                for j in range(i, N):
+                    r = -np.log(lowest_common_ancestor(terms[i], terms[j], 
+                                                       organism_name=organism_name,
+                                                       domain=domain))
+                    ss[domain][i,j] = r
+                    ss[domain][j,i] = r
 
-        return {"a": np.zeros((10,))}
+        return ss
 
     def gene_wise(self, ontology: GeneOntology, 
-                  organism_name: str) -> Dict[str, npt.NDArray]:
-        term_wise = self.term_wise()
+                  organism_name: str,
+                  domain: str) -> Dict[str, npt.NDArray]:
+        term_wise = self.term_wise(ontology, organism_name)
         
         return {"a": np.zeros((10,))}
